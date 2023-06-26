6617e2caf78d08dc16a2a9fbd3b46a6c3791b4b6
DragonDeezNoot_98 <babybangunny@gmail.com>
2023-06-26 23:29:30 +0700

    feat: Vercel analytics

--- src/components/Providers/ProviderWrapper.tsx
+++ src/components/Providers/ProviderWrapper.tsx
@@ -1,13 +1,13 @@
 import { nextSeoConfig, themeConfig } from "@/lib/configs"
 import nextFonts from "@/lib/fonts"
 import { cn } from "@/lib/utils"
+import { Analytics } from "@vercel/analytics/react"
 import { type Session } from "next-auth"
 import { SessionProvider } from "next-auth/react"
 import { DefaultSeo } from "next-seo"
 import { ThemeProvider } from "next-themes"
 import { type PropsWithChildren } from "react"
 import { Toaster } from "../Ui/toaster"
-
 type Props = PropsWithChildren<{
   session?: Session
   routerKey?: string
@@ -25,6 +25,7 @@ const ProviderWrapper = ({ session, children }: Props) => (
       >
         {children}
         <Toaster />
+        <Analytics />
       </main>
     </ThemeProvider>
   </SessionProvider>
